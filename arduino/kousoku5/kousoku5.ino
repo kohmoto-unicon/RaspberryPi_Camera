45e58d8083cfb6ca996f70bd2bfa4dfa508a9c19
河本映二 <kohmoto-e@unicon.co.jp>
2025-08-20 09:25:28 +0900

    2508200925

--- arduino/kousoku5/kousoku5.ino
+++ arduino/kousoku5/kousoku5.ino
@@ -70,7 +70,11 @@ byte commandBuffer[11];
 int commandIndex = 0;
 
 // ==== マイクロステップ設定 ====
-const int stepsPerRev = 200 * 2; // 1回転あたりのマイクロステップ数 (1/2)
+const int MICRO_STEP_1_2 = 2; // 1/2ステップ
+const int MICRO_STEP_1_4 = 4; // 1/4ステップ
+const int MICRO_STEP_1_8 = 8; // 1/8ステップ
+
+const int stepsPerRev = 200 * MICRO_STEP_1_8; // 1回転あたりのマイクロステップ数
 
 // ===================== RPM→Interval変換 =====================
 unsigned int rpmToIntervalUs(long rpm) {
